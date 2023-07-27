631e56dce793d54972416d304521aefc4a487b59
ram54118 <ram54118@gmail.com>
2023-07-27 19:05:00 +0530

    adasd

--- src/app/components/application-widget-settings/application-widget-settings.component.ts
+++ src/app/components/application-widget-settings/application-widget-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-application-widget-settings',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationWidgetSettingsComponent implements OnInit {
 
-  constructor() { }
+  ommWidgetsList = [{
+    label: 'Incoming events',
+    name: 'incomingEvents'
+  }];
+
+  alertsList = [{
+    label: 'Incoming events',
+    name: 'incomingEvents'
+  }];
+
+
+  ommWidgetsArray: any = new FormArray([]);
+  alertsArray: any = new FormArray([]);
+
+  form!: FormGroup;
+  constructor(private fomrBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.ommWidgetsList.forEach(item => {
+        const control: FormControl = new FormControl(true);
+        control.valueChanges.pipe(
+          tap(value => {
+            console.log(item);
+            console.log(value);
+          })
+        ).subscribe();
+      this.ommWidgetsArray.push(control);
+    });
+
+    this.alertsList.forEach(item => {
+      const control: FormControl = new FormControl(true);
+      control.valueChanges.pipe(
+        tap(value => {
+          console.log(item);
+          console.log(value);
+        })
+      ).subscribe();
+    this.alertsArray.push(control);
+  });
   }
 
 }
