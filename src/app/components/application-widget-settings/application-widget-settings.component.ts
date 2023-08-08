1f12eb31bddcbfba740f6b325152d400862a29eb
ram54118 <ram54118@gmail.com>
2023-08-08 12:18:50 +0530

    bar chart

--- src/app/components/application-widget-settings/application-widget-settings.component.ts
+++ src/app/components/application-widget-settings/application-widget-settings.component.ts
@@ -24,6 +24,38 @@ export class ApplicationWidgetSettingsComponent implements OnInit {
   alertsArray: any = new FormArray([]);
 
   form!: FormGroup;
+  chartData: any = [
+    {
+      stepName: 'mobileNo',
+      stepCount: 113,
+      stepDate: '07-31-2023'
+    },
+    {
+      stepName: 'mobileNo1',
+      stepCount: 13,
+      stepDate: '07-31-2023'
+    },
+    {
+      stepName: 'mobileNo2',
+      stepCount: 3,
+      stepDate: '07-31-2023'
+    },
+    {
+      stepName: 'mobileNo3',
+      stepCount: 30,
+      stepDate: '07-31-2023'
+    },
+    {
+      stepName: 'mobileNo4',
+      stepCount: 40,
+      stepDate: '07-31-2023'
+    },
+    {
+      stepName: 'mobileNo5',
+      stepCount: 90,
+      stepDate: '07-31-2023'
+    }
+  ];
   constructor(private fomrBuilder: FormBuilder) { }
 
   ngOnInit(): void {
@@ -47,7 +79,19 @@ export class ApplicationWidgetSettingsComponent implements OnInit {
         })
       ).subscribe();
     this.alertsArray.push(control);
-  });
+    });
+
+    // bar chart code starts
+    const total = Math.max(...this.chartData.map((d: any) => d.stepCount));
+    this.chartData.forEach((d: any) => {
+      d.width = this.percentage(d.stepCount, total)+'%';
+    })
+    // bar chart code ends
   }
+
+  // bar chart code starts
+  percentage(value:number, total: number) {
+    return (100 * value) / total;
+  } 
 
 }
