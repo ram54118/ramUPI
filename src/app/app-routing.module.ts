7032a52bd0edb8efb7c353f79a7fa6751be73af8
ram54118 <ram54118@gmail.com>
2023-10-25 19:08:15 +0530

    ttd component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UpiComponent } from './components/upi/upi.component';
 import { ApplicationWidgetSettingsComponent } from './components/application-widget-settings/application-widget-settings.component';
+import { TtdComponent } from './ttd/ttd.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'upi', pathMatch: 'full' },
   { path: 'upi', component: UpiComponent },
+  { path: 'ttd', component: TtdComponent },
   { path: 'settings', component: ApplicationWidgetSettingsComponent }
 ];
 
---
+++ src/app/ttd/ttd.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TtdComponent } from './ttd.component';
+
+describe('TtdComponent', () => {
+  let component: TtdComponent;
+  let fixture: ComponentFixture<TtdComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TtdComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TtdComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
